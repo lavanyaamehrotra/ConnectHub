be13dc681a48758755f374493de06bc5d53016e3
lavanyaamehrotra <144808904+lavanyaamehrotra@users.noreply.github.com>
2026-05-09 02:35:51 +0530

    chore: allow production frontend in CORS

--- ConnectHub/ConnectHub.GatewayService/Program.cs
+++ ConnectHub/ConnectHub.GatewayService/Program.cs
@@ -76,7 +76,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontends", policy =>
     {
         policy
-            .WithOrigins("http://localhost:4200", "http://localhost:8080")
+            .WithOrigins(
+                "http://localhost:4200", 
+                "http://localhost:8080",
+                "https://connecthub-frontend-f8dq.onrender.com")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
